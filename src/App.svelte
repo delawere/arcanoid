d5b4cd40471a704b37b36f714c89ac1bdcd8eb03
Zhiganov Dmitriy <delawere2008@gmail.com>
2019-09-11 20:53:12 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style>
+</style>
+
+<header>
+	<h1>Arcanoid2019</h1>
+</header>
+<main>
+	<div>
+		<canvas id="canvas" width="480" height="320"></canvas>
+	</div>
+</main>
